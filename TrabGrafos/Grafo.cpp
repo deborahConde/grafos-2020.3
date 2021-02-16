5feeb512842de868611027461167e7508469726a
deborahConde <deborahsc23@gmail.com>
2021-02-16 11:10:40 -0300

    Add files via upload

--- TrabGrafos/Grafo.cpp
+++ TrabGrafos/Grafo.cpp
@@ -47,7 +47,7 @@ int Grafo::getNumeroArestas() {
 	return this->numero_arestas;
 }
 
-//Funcao que verifica se o grafo é direcionado
+//Funcao que verifica se o grafo ? direcionado
 bool Grafo::getDirecionado()
 {
 	return this->direcionado;
@@ -233,8 +233,88 @@ Grafo* getVertexInduced(int* listIdNodes) {
 }
 
 Grafo* agmKuskal() {
+	
+
+
+
 	return nullptr;
 }
-Grafo* agmPrim() {
-	return nullptr;
+Grafo* agmPrim(Grafo *grafo)
+{
+
+    /// verifica se um grafo eh conexo.
+        auto *arvoreGeradoraMinima = new Grafo();
+
+        unsigned int tam = grafo->listaNo.size();
+
+        /// pega um v?rtice aleat?rio e o insere na AGM.
+        auto *noInicial = new No(grafo->listaNo.at(rand() % tam-1)->id);
+        arvoreGeradoraMinima->listaNo.push_back(noInicial);
+
+        /// adiciona n?s e arestas de menor peso enquanto a AGM n?o possuir todos os v?rtices.
+        while (arvoreGeradoraMinima->listaNo.size() < tam)
+        {
+            Aresta *aresta = getArestaMenorPeso(grafo, arvoreGeradoraMinima);
+
+            auto *No1 = new No(aresta->no1->id);
+            auto *No2 = new No(aresta->no2->id);
+
+            if (!arvoreGeradoraMinima->verificaId(No1->id))
+            {
+                arvoreGeradoraMinima->listaNo.push_back(No1);
+            }
+
+            if (!arvoreGeradoraMinima->verificaId(No2->id))
+            {
+                arvoreGeradoraMinima->listaNo.push_back(No2);
+            }
+
+            arvoreGeradoraMinima->arestas.push_back(aresta);
+        }
+
+        imprimeSolucao(arvoreGeradoraMinima);
+}
+
+/**
+ * Retorna a aresta de menor peso de um grafo
+ * @param Grafo
+ * @param Grafo
+ * @return Aresta
+*/
+Aresta *Grafo::getArestaMenorPeso(Grafo *grafo, Grafo *arvoreGeradoraMinima)
+{
+    auto *arestaMenorPeso = new Aresta();
+    int pesoMinimo = numeric_limits<int>::max();
+
+    for (auto aresta : grafo->arestas)
+    {
+        if (!(arvoreGeradoraMinima->verificaId(aresta->no1->id) && arvoreGeradoraMinima->verificaId(aresta->no2->id)))
+        {
+            if (arvoreGeradoraMinima->verificaId(aresta->no1->id)|| arvoreGeradoraMinima->verificaId(aresta->no2->id))
+            {
+                if (aresta->peso < pesoMinimo)
+                {
+                    arestaMenorPeso = aresta;
+                    pesoMinimo = aresta->peso;
+                }
+            }
+        }
+    }
+
+    return arestaMenorPeso;
+}
+
+/**
+ * Imprime uma ?rvore geradora m?nima de um grafo
+ * @param Grafo
+ * @return void
+*/
+void Prim::imprimeSolucao(Grafo *grafo)
+{
+    cout << "Arvore Geradora Minima - Prim:" << endl;
+    for (auto aresta : grafo->arestas)
+    {
+        cout << "(" << aresta->no1->id << "," << aresta->no2->id << ") ";
+    }
+    cout << endl;
 }
